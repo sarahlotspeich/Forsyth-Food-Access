026509bcb85e878cc6e799a286219ed328451257
Sarah Lotspeich (She/Her) <sarah.lotspeich@gmail.com>
2023-07-11 08:43:36 -0400

    Delete set_api_keys.R

--- data/set_api_keys.R
+++
@@ -1,8 +0,0 @@
-# Set API key for tidycensus
-tidycensus::census_api_key("0f3b62c912d5ea731c00bd1d8b6ed2bacedb8040")
-
-# Set API key for googleway
-googleway::set_key("AIzaSyBHtJb_aWUZBtjvMNj3b0Jeg9Y4okjkkv4")
-
-# Set API key for ggmap
-ggmap::register_google("AIzaSyBHtJb_aWUZBtjvMNj3b0Jeg9Y4okjkkv4")
